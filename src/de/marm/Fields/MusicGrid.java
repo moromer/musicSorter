e61b0043b4a3889e6ccc9a6f67b24a94b65450fe
Mario Romer <mario.romer@gmx.net>
2013-10-26 20:12:38 +0200

    changed GridPanel to TabelPanel with Dummy Data from Type Music added new class Music. This class stores all relevant data - interpret - album - title - path

--- src/de/marm/Fields/MusicGrid.java
+++ src/de/marm/Fields/MusicGrid.java
@@ -1,26 +1,68 @@
 package de.marm.Fields;
 
+import java.util.ArrayList;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+import de.marm.Typ.Music;
 
 public class MusicGrid {
 	
-	private Group musicGroup;
+	private Table table;
+	private ArrayList<TableItem> items;
 
 	@SuppressWarnings("unused")
 	private MusicGrid() {
 		
 	}
 	
+	
+	public void addData(Music music) {
+		TableItem item = new TableItem(table, SWT.None);
+		item.setText(music.getData());
+		this.items.add(item);
+	}
+	
 	private void createMusicGrid(Composite parent, String description, Color bg, GridData grid) {
-		musicGroup = new Group(parent, SWT.NONE);
-		musicGroup.setBackground(bg);
-		musicGroup.setText(description);
+		table = new Table(parent, SWT.BORDER);
+		table.setLayoutData(grid);
+		System.out.println(parent.getLayout().toString());
 		
-		musicGroup.setLayoutData(grid);
+		TableColumn cInterpret = new TableColumn(table, SWT.CENTER);
+		cInterpret.setText("Interpret");
+		cInterpret.setWidth(200);
+		
+		TableColumn cAlbum = new TableColumn(table, SWT.CENTER);
+		cAlbum.setText("Album");
+		cAlbum.setWidth(200);
+		
+		TableColumn cTitle = new TableColumn(table, SWT.CENTER);
+		cTitle.setText("Title");
+		cTitle.setWidth(200);
+		
+		table.setHeaderVisible(true);
+		
+		items = new ArrayList<TableItem>();
+		
+		//Dummy Data
+		Music m1 = new Music("Linken Park", "Livint Things", "Burn it Down");
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		this.addData(m1);
+		
 	}
 	
 	public MusicGrid(Composite parent, String description, Color bg) {
---
+++ src/de/marm/Typ/Music.java
@@ -0,0 +1,61 @@
+package de.marm.Typ;
+
+public class Music {
+	private String interpret;
+	private String album;
+	private String title;
+	private String path;
+
+	public Music(String interpret, String album, String title) {
+		this.interpret = interpret;
+		this.album     = album;
+		this.title     = title;
+	}
+	
+	public Music(String interpret, String album, String title, String path) {
+		this.interpret = interpret;
+		this.album     = album;
+		this.title     = title;
+		this.path      = path;
+	}
+	
+	public String[] getData() {
+		return new String[] {this.interpret, this.album, this.title};
+	}
+
+	public String getInterpret() {
+		return interpret;
+	}
+
+	public void setInterpret(String interpret) {
+		this.interpret = interpret;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+}
+
