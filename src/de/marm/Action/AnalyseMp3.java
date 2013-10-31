5cca4514977d8c8a6bd6b7eced7587820d5fa929
Mario Romer <mario.romer@gmx.net>
2013-10-31 19:28:38 +0100

    added tests for Music Class
    read all Files in defined folder

--- src/de/marm/Action/AnalyseMp3.java
+++ src/de/marm/Action/AnalyseMp3.java
@@ -4,16 +4,61 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class AnalyseMp3 {
 	private String path;
-
+	private ArrayList<File> fileList = null;
+ 
 	public AnalyseMp3(String path){
+		if(!path.matches("^.*/$")) {
+			path += "/";
+		}
 		this.path = path;
 	}
 	
 	public void startAnalyze() throws FileNotFoundException{
 		File file = new File(path);
 		DataInputStream dis = new DataInputStream(new FileInputStream(file));
+	}
+	
+	public File getFolder() {
+		return new File(this.path);
+	}
+	
+	private void readFiles() {
+		
+		if(this.fileList == null) {
+			this.fileList = new ArrayList<File>();
+		}
+		
+		File folder = new File(this.path);
+		
+		for(File fileEntry : folder.listFiles()) {
+			if(fileEntry.isDirectory()) {
+				this.readFiles();
+			} else {
+				this.fileList.add(fileEntry);
+			}
+		}
+		
+	}
+	
+	public ArrayList<File> getFileList(){
+		if(this.fileList == null) {
+			this.readFiles();
+		}
+		return this.fileList;
+	}
+	
+	
+	public String getPath() {
+		return this.path;
+	}
+	
+	public void readInFiles() {
+		this.fileList.removeAll(fileList);
+		this.readFiles();
+		
 	}
 }
---
+++ src/de/marm/Tests/Action/AnalyseMp3.java
@@ -0,0 +1,97 @@
+package de.marm.Tests.Action;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnalyseMp3 {
+	
+	private de.marm.Action.AnalyseMp3 analyze;
+	private String expectedPath;
+
+	@Before
+	public void prepare() {
+		expectedPath = "/home/marm/Music/";
+		analyze = new de.marm.Action.AnalyseMp3(expectedPath);
+	}
+	
+	@Test
+	public void test() {
+		assertEquals("check initialization",de.marm.Action.AnalyseMp3.class, analyze.getClass());
+		assertEquals("check path",expectedPath, analyze.getPath());
+		
+		this.resetAnalyzeObject("/home/marm/Music");
+		
+		assertEquals("check initialization",de.marm.Action.AnalyseMp3.class, analyze.getClass());
+		assertEquals("check path",expectedPath, analyze.getPath());
+	}
+	
+	@Test
+	public void checkFileReader() throws IOException {
+		String tmpPath = this.expectedPath + "tmp/";
+		rmdir(tmpPath);
+		
+		File tmpFolder = new File(tmpPath);
+		
+    	tmpFolder.mkdir();
+    	this.createFile(tmpPath, "File1.mp3");
+    	
+		this.resetAnalyzeObject(tmpPath);
+		
+		ArrayList<File> fileList = analyze.getFileList();
+		
+		assertEquals("check count of files", 1, fileList.size());
+		assertEquals("check File", "File1.mp3", fileList.get(0).getName());
+		
+		
+		this.createFile(tmpPath, "File2.mp3");
+		fileList = analyze.getFileList();
+		
+		assertEquals("check count of files is still 1", 1, fileList.size());
+		
+		assertEquals("check File is still File1.mp3", "File1.mp3", fileList.get(0).getName());
+		
+		analyze.readInFiles();
+		
+		assertEquals("check count of files is now 2", 2, fileList.size());
+		assertEquals("check File is still File1.mp3", "File1.mp3", fileList.get(0).getName());
+		
+		
+		rmdir(tmpPath);
+		
+		
+	}
+	
+	private void createFile(String path, String fileName) throws IOException {
+		File newFile = new File(path + fileName);
+		
+		if(!newFile.exists()) {
+			newFile.createNewFile();
+		}
+	}
+	
+	private void resetAnalyzeObject(String path) {
+		analyze = null;
+		analyze = new de.marm.Action.AnalyseMp3(path);
+		
+	}
+	
+	private void rmdir(String path){
+		File folder = new File(path);
+		
+		if(folder.isDirectory()) {
+			for(File file : folder.listFiles()) {
+				rmdir(path+file.getName());
+			}
+		}
+		
+		folder.delete();
+	}
+	
+
+}
